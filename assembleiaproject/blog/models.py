2b3cacf5b79a4ce9bd9fcdbcc05228a36a0a1dc3
Helton Souza <helton.souza@ongame.com.br>
2022-06-09 17:03:25 -0300

    atualizar branch

--- assembleiaproject/blog/models.py
+++ assembleiaproject/blog/models.py
@@ -11,24 +11,28 @@ class PublishedManager(models.Manager):
 
 # Create your models here.
 class Post(models.Model):
-    Choose_status = (
+    STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published')
     )
     title = models.CharField(_('title'), max_length=250)
     slug = models.SlugField(_('slug'), max_length=250)
-    author = models.ForeignKey(_('author'), settings.AUTH_USER_MODEL,
-                               on_delete=models.CASCADE,
-                               related_name='blog_posts')
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,
+                               on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField(_('body'))
     publish = models.DateTimeField(_('publish'), default=timezone.now)
     img = models.ImageField(upload_to='blog_img')
     created = models.DateTimeField(_('created'), auto_now_add=True)
     updated = models.DateTimeField(_('updated'), auto_now=True)
+    status = models.CharField(_('status'), max_length=10, choices=STATUS_CHOICES, default='draft')
 
     class Meta:
+        ordering = ('-publish', )
         verbose_name = _('Post')
         verbose_name_plural = _('Posts')
 
     def __str__(self):
         return self.title
+
+    objects = models.Manager()
+    published = PublishedManager()
--- assembleiaproject/blog/views.py
+++ assembleiaproject/blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Post
+
 
 # Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    context = {'posts': posts}
+
+    return render(request, '', context)
+
+
+def post_detail(request, day, year, month, post):
+    post = get_object_or_404(Post, slug=post,
+                             status='publised',
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
+    context = {'post': post}
+    return render(request, '', context)
