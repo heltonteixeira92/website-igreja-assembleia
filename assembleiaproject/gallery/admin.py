0c78309656aa69797b24fe32a4d7dcb8a12508be
Helton Souza <helton.souza@ongame.com.br>
2022-06-22 11:07:18 -0300

    some changes

--- assembleiaproject/gallery/admin.py
+++ assembleiaproject/gallery/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
-from .models import Image
+from .models import Image, Album
 
 
 # Register your models here.
 @admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-    list_display = ['title', 'slug', 'img', 'created']
-    list_filter = ['created']
+    list_display = ('title', 'slug', 'img', 'created')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('created', )
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug')
+    prepopulated_fields = {'slug': ('title',)}
--- assembleiaproject/gallery/models.py
+++ assembleiaproject/gallery/models.py
@@ -4,17 +4,40 @@ from django.utils.text import slugify
 
 
 # Create your models here.
+class Album(models.Model):
+    title = models.CharField(_('title'), max_length=128)
+    slug = models.SlugField(_('slug'), max_length=128)
+
+    class Meta:
+        ordering = ('title', )
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+
 class Image(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
         ('published', 'Published')
     )
+    album = models.ForeignKey(Album, on_delete=models.CASCADE,
+                              related_name='album_image',
+                              verbose_name=_('album'),
+                              null=True)
     title = models.CharField(_('title'), max_length=128)
     slug = models.SlugField(_('slug'), max_length=128, blank=True)
     img = models.ImageField(upload_to='images/%Y/%m/%d/')
     description = models.TextField(_('description'), blank=True)
     created = models.DateTimeField(_('created'), auto_now_add=True, db_index=True)
     status = models.CharField(_('status'), max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('album', 'title')
 
     def __str__(self):
         return self.title
