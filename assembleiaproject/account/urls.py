6f1618ab68855dd224b8140cbb2e5e3aeb45e5d9
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2022-06-25 01:38:40 -0300

    login/logout

---
+++ assembleiaproject/account/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import user_login
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('login/', user_login, name='login'),
+    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
