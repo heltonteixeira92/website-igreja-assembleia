b80f20b46b9e8e8e762da63dac916b67981a7747
Helton Teixeira <heltonteixeiradesouza@hotmail.com>
2022-06-26 21:18:10 -0300

    error page 500 and 404

--- assembleiaproject/base/views.py
+++ assembleiaproject/base/views.py
@@ -24,3 +24,15 @@ def contact_us(request):
 
 def about_us(request):
     return render(request, 'about-us.html')
+
+
+def handler500(request):
+    response = render(request, 'errors/error-500.html')
+    response.status_code = 500
+    return response
+
+
+def handler404(request, exception):
+    response = render(request, 'errors/error-404.html')
+    response.status_code = 404
+    return response
