951cda6e6b386fb9351597db3637696a2907bd02
Helton Souza <helton.souza@ongame.com.br>
2022-07-18 16:54:14 -0300

    safe string to show correctly in html

--- assembleiaproject/blog/admin.py
+++ assembleiaproject/blog/admin.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
+# from django.urls import reverse
+
 from .models import Post
 
 
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
+
     list_display = ('title', 'slug', 'author', 'publish', 'status')
     list_filter = ('status', 'created', 'publish', 'author')
     search_fields = ('title', 'body')
@@ -12,3 +15,13 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ('author',)
     date_hierarchy = 'publish'
     ordering = ('status', 'publish')
+    # exclude = 'slug',
+
+    # @staticmethod
+    # def view_on_site(obj):
+    #     return reverse('blog:post_detail', kwargs={'year': obj.publish.year, 'month': obj.publish.month,
+    #                                                'post': obj.slug})
+
+    def save_model(self, request, obj, form, change):
+        obj.author = request.user
+        obj.save()
